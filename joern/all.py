e43ebf3074dc9422afcc4eb15daccbf99afa9c13
fabs <fyamagu@gwdg.de>
2013-12-09 11:50:17 +0100

    Introduced method to set the database URL, removed unused function. Renamed executeGremlinCmd to runGremlinQuery and executeCypherCmd to runCypherQuery.

--- joern/all.py
+++ joern/all.py
@@ -1,19 +1,28 @@
 from py2neo import neo4j, gremlin, cypher
 import os
 
+DEFAULT_GRAPHDB_URL = "http://localhost:7474/db/data/"
+
 class JoernSteps:
 
     def __init__(self):
         self._initJoernSteps()
+    
+    def setGraphDbURL(self, url):
+        self.graphDbURL = url
         
+    def connectToDatabase(self):
+        self.graphDb = neo4j.GraphDatabaseService(self.graphDbURL)
+    
     def _initJoernSteps(self):
-        self.graphDb = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-   
-        joernStepsDir = os.path.dirname(__file__) + '/joernsteps/'
-        self.initCommand = self._createInitCommand(joernStepsDir)
+        self.graphDbURL = DEFAULT_GRAPHDB_URL
         
-    def _createInitCommand(self, stepsDir):
+        self.initCommand = self._createInitCommand()
+
+    def _createInitCommand(self):
         
+        stepsDir = os.path.dirname(__file__) + '/joernsteps/'
+
         initCommand = ""
         for (root, dirs, files) in os.walk(stepsDir):
             files.sort()
@@ -22,14 +31,11 @@ class JoernSteps:
                 initCommand += file(filename).read() + "\n"
         return initCommand
         
-    def executeGremlinScript(self, filename):
-        return self.executeGremlinCmd()
-
-    def executeGremlinCmd(self, cmd):
+    def runGremlinQuery(self, cmd):
         finalCmd = self.initCommand
         finalCmd += cmd
         return gremlin.execute(finalCmd, self.graphDb)
         
-    def executeCypherQuery(self, query):
-        return cypher.execute(self.graphDb, query)
+    def runCypherQuery(self, cmd):
+        return cypher.execute(self.graphDb, cmd)
     
