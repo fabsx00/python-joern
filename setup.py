5e5982a229251638c8d07e8ecf8f73447eb4cfa5
fabs <fyamagu@gwdg.de>
2013-12-13 12:29:35 +0100

    Added version information to py2neo-gremlin dependency.

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
     long_description = read('README.md'),
     packages = find_packages(),
     data_files = datafiles,
-    install_requires = ['py2neo-gremlin'],
-    dependency_links = ['https://github.com/fabsx00/py2neo-gremlin/tarball/master/#egg=py2neo-gremlin']
+    install_requires = ['py2neo-gremlin == 0.1'],
+    dependency_links = ['https://github.com/fabsx00/py2neo-gremlin/tarball/master/#egg=py2neo-gremlin-0.1']
 )
