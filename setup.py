24099cdc783fd2b47029b5f57dbbb6eba19a783d
fabs <fyamagu@gwdg.de>
2013-12-13 16:33:20 +0100

    Fix for setup.py.

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     url = "http://github.com/fabsx00/",
     long_description = read('README.md'),
     packages = find_packages(),
+    package_data={"joern": ['joernsteps/*',]},
     install_requires = ['py2neo-gremlin == 0.1'],
     dependency_links = ['https://github.com/fabsx00/py2neo-gremlin/tarball/master/#egg=py2neo-gremlin-0.1']
 )
