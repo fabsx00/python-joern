840cf229190290639ed793b48d2e9cb41d7af0ee
fabs <fyamagu@gwdg.de>
2013-12-13 16:23:20 +0100

    Made 'joernsteps' a package as opposed to including it as a data directory as that did not work via pip.

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
-import os, glob
+import os
 
 from setuptools import setup, find_packages
-
-datadir = os.path.join('joern','joernsteps')
-datafiles = [(datadir, [f for f in glob.glob(os.path.join(datadir, '*'))])]
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -18,7 +15,6 @@ setup(
     url = "http://github.com/fabsx00/",
     long_description = read('README.md'),
     packages = find_packages(),
-    data_files = datafiles,
     install_requires = ['py2neo-gremlin == 0.1'],
     dependency_links = ['https://github.com/fabsx00/py2neo-gremlin/tarball/master/#egg=py2neo-gremlin-0.1']
 )
