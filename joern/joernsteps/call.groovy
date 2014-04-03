0fddf463efc26ec63eaf4a9f9cac6bc4c6065712
fabs <fyamagu@gwdg.de>
2014-04-03 11:33:29 +0200

    Additional utility steps for traversals in calls.

--- joern/joernsteps/call.groovy
+++ joern/joernsteps/call.groovy
@@ -3,11 +3,21 @@
    (Optimized) Match-traversals for Calls.
 */
 
-Gremlin.defineStep('ithArguments', [Vertex,Pipe], { i -> 
+Gremlin.defineStep('callToArguments', [Vertex, Pipe], {
 	 _().children().filter{it.type == TYPE_ARGLIST}
-	 .children().filter{ it.childNum == i }
+	 .children()
 })
+
+Gremlin.defineStep('ithArguments', [Vertex,Pipe], { i -> 
+	_().callToArguments()
+	.filter{ it.childNum == i }
+})
+
 
 Gremlin.defineStep('argToCall', [Vertex, Pipe], {
 	_().in(AST_EDGE).in(AST_EDGE)
+})
+
+Gremlin.defineStep('calleeToCall', [Vertex, Pipe], {
+	_().in(AST_EDGE)
 })
