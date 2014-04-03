65e5d8c7e60f049f541a1fa0d83d4a61348b9048
fabs <fyamagu@gwdg.de>
2014-04-03 18:50:20 +0200

    Test for condition nodes in conditional expressions.

--- test.py
+++ test.py
@@ -104,6 +104,16 @@ class UDGTests(PythonJoernTests):
         x = self.j.runGremlinQuery(query)
         self.assertEquals(x[0], 'pLtv -> u . u16')
 
+    def testComplexAssign(self):
+        
+        query = """getFunctionASTsByName('conditional_expr')
+        .astNodes().filter{ it.type == 'Condition'}
+        .uses()
+        .code
+        """
+        x = self.j.runGremlinQuery(query)
+        self.assertEquals(len(x), 1)
+
 
 class DataFlowTests(PythonJoernTests):
     
