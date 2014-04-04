52eb9598bc737920777953f3a9ba11f1c377d979
fabs <fyamagu@gwdg.de>
2014-04-04 12:00:34 +0200

    Added functionToASTNodes.

--- joern/joernsteps/function.groovy
+++ joern/joernsteps/function.groovy
@@ -7,6 +7,10 @@ Gremlin.defineStep("functionToAST", [Vertex,Pipe], {
 	_().out(FUNCTION_TO_AST_EDGE)
 })
 
+Gremlin.defineStep("functionToASTNodes", [Vertex,Pipe], {
+	_().functionToAST().astNodes()
+})
+
 Gremlin.defineStep("functionToStatements", [Vertex,Pipe],{
 	_().transform{ queryNodeIndex('isCFGNode:True AND functionId:' + it.id) }
 	 .scatter()
