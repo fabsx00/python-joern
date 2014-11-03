426b06192b0d1fda19a85355a9143078b74c216c
fabs <fyamagu@gwdg.de>
2014-11-03 12:11:13 +0100

    toExitNode.

--- joern/joernsteps/interproc.groovy
+++ joern/joernsteps/interproc.groovy
@@ -1,29 +1,93 @@
 
 import java.util.regex.Pattern;
 
-/**
-   For a given argument node, determine direct initializers, i.e., 
-   the last statements, which tainted any of the variables.
-*/
+/******************************************************
+ * Steps for interprocedural analysis
+ * Experimental and subject to change.
+ ******************************************************/
 
-Gremlin.defineStep('argToInitNodes', [Vertex, Pipe], {
+/**
+ * For a given call-expression, determine whether it's `argNum`'th
+ * argument is tainted by a statement matching the source description.
+ * */
+
+Gremlin.defineStep('taintedArg', [Vertex, Pipe], { argNum, src = { [1]._() }, N_LOOPS = 4 ->
+	_().filter{
+		argIsTainted(it, argNum, src, N_LOOPS)
+	}
+})
+
+Object.metaClass.argIsTainted = { node, argNum, src, N_LOOPS = 2 ->
 	
-	_().argToInitNodesLocal()
+	node.ithArguments(argNum)
+	.as('y').expandParameters().tainted().dedup()
+	.loop('y'){ it.loops <= N_LOOPS && (src(it.object).toList() == [] || src(it.object).toList() == [1] ) }
+	{true}
+	// {  src(it.object).toList() == [10] }
+	.filter{ src(it).toList() != [] }
+	.toList() != []
+}
+
+/**
+ * For a given node, obtain producers for
+ * all symbols it uses.
+ * TODO: Consider renaming this to `allProducers`
+ **/
+
+Gremlin.defineStep('tainted', [Vertex, Pipe], {
+	_().transform{
+		it.producers(it.uses().code.toList())
+	}.scatter()
+})
+
+
+/**
+ * Identity for non-parameters.
+ * For parameters, expand into caller arguments.
+ **/
+
+Gremlin.defineStep('expandParameters', [Vertex, Pipe], {
 	
-	.ifThenElse{ it.type == 'Parameter'}
-	{ it.parameterToCallerArgs().argToInitNodes().scatter() }
-	{ it }
+	_().transform{
+	  if(it.type == 'Parameter'){
+		def l = it.parameterToCallerArgs().toList();
+		if(l != []) l else it._().toList()
+	  }
+	  else
+	   it._().toList()
+	}.scatter()
+
 })
 
 /**
-  For a given parameter, get all nodes of arguments of callers.
+ For a given parameter, get all nodes of arguments of callers.
 */
 
 Gremlin.defineStep('parameterToCallerArgs', [Vertex, Pipe], {
+   _().transform{
+	   paramNum = it.childNum;
+	   funcName = it.functions().name.toList()[0];
+	   getCallsTo(funcName).ithArguments(paramNum)
+   }.scatter()
+})
+
+
+/**
+ * Identity for nodes that do not contain arguments.
+ * For arguments, descend into called function
+ * parameters.
+ **/
+
+Gremlin.defineStep('expandArguments', [Vertex, Pipe], {
 	_().transform{
-		paramNum = it.childNum;
-		funcName = it.functions().name.toList()[0];
-		getCallsTo(funcName).ithArguments(paramNum)
+	  
+	  def args = it.match{ it.type == "Argument"}.toList()
+	  
+	  if(args != []){
+			def l = args._().argToParameters().toList();
+			if(l != []) l else it._().toList()
+	  }else
+	   it._().toList()
 	}.scatter()
 })
 
@@ -43,48 +107,30 @@ Gremlin.defineStep('argToParameters', [Vertex, Pipe], {
 
 
 /**
-  For a given argument node, determine local initializers.
-  These may be parameters of the function.
-*/
+ * For a given call-site, return statements in the callee
+ * that taint arguments, i.e., assign to the function's parameters.
+ * */
 
-Gremlin.defineStep('argToInitNodesLocal', [Vertex, Pipe], {
-	
-	_().sideEffect{ stmtId = it.statements().id.toList()[0] }	
-	.sliceBackFromArgument(1, ["REACHES"])
-	.filter{ it.id != stmtId }
+Gremlin.defineStep('argTainters', [Vertex,Pipe], {
+	def params = _().taintedArguments().expandArguments()
+		
 })
 
+/**
+ * For a given call-site, return arguments that are tainted.
+ * */
+
+Gremlin.defineStep('taintedArguments', [Vertex, Pipe], {
+	_().callToArguments().filter{ it.defines().toList() != [] }
+})
+
+/**
+ * For a function node, get callers using `name` property.
+ **/
 
 Gremlin.defineStep('functionToCallers', [Vertex,Pipe], {
 	_().transform{
 		getCallsTo(it.name)
-	}.scatter()
-})
-
-Gremlin.defineStep('expandParameters', [Vertex, Pipe], {  
-	
-	_().transform{
-	  if(it.type == 'Parameter'){
-	    def l = it.parameterToCallerArgs().toList();
-	    if(l != []) l else it._().toList()
-	  }	  
-	  else
-	   it._().toList()
-	}.scatter()
-
-})
-
-Gremlin.defineStep('expandArguments', [Vertex, Pipe], {
-	_().transform{
-	  
-	  def args = it.match{ it.type == "Argument"}.toList()	  	  
-	  
-	  if(args != []){
-	        def l = args._().argToParameters().toList();
-               
-		if(l != []) l else it._().toList()
-	  }else
-	   it._().toList()
 	}.scatter()
 })
 
@@ -112,61 +158,20 @@ Gremlin.defineStep('iUnsanitized', [Vertex,Pipe], { sanitizer, src = { [1]._() }
 	}.scatter()
 })
 
-Gremlin.defineStep('iUnsanitizedPaths', [Vertex,Pipe], { sanitizer, src = { [1]._() }, N_LOOPS = 4 ->
-		  
-  _().transform{
-	  
-	  nodes = getNodesToSrc(it, src, N_LOOPS)
-	  finalNodes = nodes.findAll{ it[1] == true}.collect{ it[0] }.unique()
-	  nodes = nodes.collect{ it[0] }.unique()
-	  srcChecker = { node -> if(node.id in nodes) [10] else [] }
-	  
-	  it.sideEffect{ d = [:]; rootNode = null; }
-	  .as('x').expandParameters()
-	  .unsanitizedPaths(sanitizer, srcChecker).dedup()
-	  .transform{
-		  def path = it.toList()
-		  if(!rootNode && path.size() != 0) rootNode = path[-1]
-		  d[path[-1]] = (d[path[-1]] ?: []).plus([path]);
-	  	  path[0]
-	  }
-	  .loop('x'){ it.loops <= N_LOOPS && (src(it.object).toList() == [] || src(it.object).toList() == [1] ) }
-	  {src(it.object).toList() != [] && (it.object.id in finalNodes) }
-	  .transform{
-		  dict2List(d, rootNode)
-	  }
-	  
-  } //.scatter()
-})
 
-Object.metaClass.dict2List = { d, node ->
-	if(!d[node])
-		return [[node, []]]
-	
-	def retval = [[node, d[node]]]
-	d[node].each{
-		retval.add(dict2List(d, it[0]))
-	}
-	retval
-}
-
-
-Gremlin.defineStep('taintedArg', [Vertex, Pipe], { argNum, src = { [1]._() }, N_LOOPS = 4 ->
-
-	_().filter{
-	  argIsTainted(it, argNum, src, N_LOOPS)
-	}
-})
+/**
+ 	(Called by iUnsanitized)
+ 	Starting from a sink-node 'it' and for a given
+	source-description 'sourceDescription', find all
+	source nodes that match the source description
+	even across the boundaries of functions.
+	Elements in the returned list are pairs of the form
+	[id, isFinalNode] where 'id' is the node's id and
+	isFinalNode indicates whether no further expansion
+	of this node was performed.
+ **/
 
 Object.metaClass.getNodesToSrc = { it, sourceDescription, N_LOOPS ->	
-	// Starting from a sink-node 'it' and for a given
-	// source-description 'sourceDescription', find all
-	// source nodes that match the source description
-	// even across the boundaries of functions.
-	// Elements in the returned list are pairs of the form
-	// [id, isFinalNode] where 'id' is the node's id and
-	// isFinalNode indicates whether no further expansion
-	// of this node was performed.
 	
 	_getNodesToSrc(it, sourceDescription, 0, N_LOOPS).unique()
 }
@@ -186,7 +191,6 @@ Object.metaClass._getNodesToSrc = { it, src, depth, N_LOOPS ->
 			return []
 	}
 	
-	
 	def children = it._().expandParameters().tainted().toList()
 	
 	def x = children.collect{ child ->
@@ -200,30 +204,6 @@ Object.metaClass._getNodesToSrc = { it, src, depth, N_LOOPS ->
 	else
 		return x.plus([[it.id, false]])
 }
-
-Object.metaClass.argIsTainted = { node, argNum, src, N_LOOPS = 2 ->
-	
-	node.ithArguments(argNum)
-	.as('y').expandParameters().tainted().dedup()
-	.loop('y'){ it.loops <= N_LOOPS && (src(it.object).toList() == [] || src(it.object).toList() == [1] ) }
-	{true}
-	// {  src(it.object).toList() == [10] }
-	.filter{ src(it).toList() != [] }
-	.toList() != []
-	
-}
-
-Gremlin.defineStep('tainted', [Vertex, Pipe], {
-	_().transform{
-		it.producers(it.uses().code.toList())
-	}.scatter()
-})
-  
-
-Gremlin.defineStep('nonEmpty', [Vertex,Pipe], { closure ->
-	_().filter{ closure(it).toList() != [] }
-})
-
 
 Gremlin.defineStep('checks', [Vertex,Pipe], { regex ->
 		
@@ -275,7 +255,6 @@ Object.metaClass.flattenByOne = { lst ->
 }
 
 
-
 NO_RESTRICTION = { a,s -> []}
 ANY_SOURCE = { [1]._() }
 
@@ -293,3 +272,79 @@ Object.metaClass.sourceMatches = { regex ->
   }
 }
 
+/** Unused right now */
+
+/**
+ For a given argument node, determine direct initializers, i.e.,
+ the last statements, which tainted any of the variables
+ within this function or a caller. Note, that this traversal
+ DOES NOT enter the callee.
+  
+*/
+
+Gremlin.defineStep('argToInitNodes', [Vertex, Pipe], {
+  
+  _().argToInitNodesLocal()
+  
+  .ifThenElse{ it.type == 'Parameter'}
+  { it.parameterToCallerArgs().argToInitNodes().scatter() }
+  { it }
+})
+
+/**
+ For a given argument node, determine local initializers.
+ These may be parameters of the function.
+*/
+
+Gremlin.defineStep('argToInitNodesLocal', [Vertex, Pipe], {
+   
+   _().sideEffect{ stmtId = it.statements().id.toList()[0] }
+   .sliceBackFromArgument(1, ["REACHES"])
+   .filter{ it.id != stmtId }
+})
+
+/**
+ * Version of iUnsanitized that returns paths.
+ * */
+
+Gremlin.defineStep('iUnsanitizedPaths', [Vertex,Pipe], { sanitizer, src = { [1]._() }, N_LOOPS = 4 ->
+		  
+  _().transform{
+	  
+	  nodes = getNodesToSrc(it, src, N_LOOPS)
+	  finalNodes = nodes.findAll{ it[1] == true}.collect{ it[0] }.unique()
+	  nodes = nodes.collect{ it[0] }.unique()
+	  srcChecker = { node -> if(node.id in nodes) [10] else [] }
+	  
+	  it.sideEffect{ d = [:]; rootNode = null; }
+	  .as('x').expandParameters()
+	  .unsanitizedPaths(sanitizer, srcChecker).dedup()
+	  .transform{
+		  def path = it.toList()
+		  if(!rootNode && path.size() != 0) rootNode = path[-1]
+		  d[path[-1]] = (d[path[-1]] ?: []).plus([path]);
+			path[0]
+	  }
+	  .loop('x'){ it.loops <= N_LOOPS && (src(it.object).toList() == [] || src(it.object).toList() == [1] ) }
+	  {src(it.object).toList() != [] && (it.object.id in finalNodes) }
+	  .transform{
+		  dict2List(d, rootNode)
+	  }
+	  
+  } //.scatter()
+})
+
+Object.metaClass.dict2List = { d, node ->
+	if(!d[node])
+		return [[node, []]]
+	
+	def retval = [[node, d[node]]]
+	d[node].each{
+		retval.add(dict2List(d, it[0]))
+	}
+	retval
+}
+
+Gremlin.defineStep('nonEmpty', [Vertex,Pipe], { closure ->
+	_().filter{ closure(it).toList() != [] }
+})
